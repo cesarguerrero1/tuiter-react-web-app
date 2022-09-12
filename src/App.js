0d2f4e978c76ef0b21c0b7055d6acc20ffd4e9fc
CGuerrero <enginestaff@cesars-mbp.myfiosgateway.com>
2022-09-11 22:50:42 -0400

    Played with App.js to make sure updates are being reflected

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          It seems that my website is working!
         </p>
         <a
           className="App-link"
