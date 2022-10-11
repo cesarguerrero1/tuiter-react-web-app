944174056bb41eadfb0b4956e205d1a60c33ca0f
Cesar Guerrero <cguerrero.cesar@gmail.com>
2022-10-10 22:21:03 -0400

    Lab 2.8

--- public/labs/a5/index.js
+++ public/labs/a5/index.js
@@ -196,4 +196,25 @@ console.log("Filter Function");
 console.log(numbersGreaterThan2);
 console.log(evenNumbers);
 console.log(oddNumbers);
+console.log('');
+
+//Template Strings
+console.log("Template Strings");
+const five = 2 + 3;
+const result1 = "2 + 3 = " + five;
+console.log(result1);
+
+const result2 = `2 + 3 = ${2 + 3}`;
+console.log(result2);
+
+const username = "alice";
+const greeting1 = `Welcome home ${username}`;
+console.log(greeting1);
+
+loggedIn = false;
+const greeting2 = `Logged in: ${loggedIn ? "Yes" : "No"}`;
+console.log(greeting2)
+
+//Using Javascropt it manipulat the DOM
+
 
