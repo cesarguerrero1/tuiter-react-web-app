7c85cfbcc4c3b6c8a74fa8c4092ee73ce19bf162
Cesar Guerrero <cguerrero.cesar@gmail.com>
2022-10-16 02:05:38 -0400

    Finished 2.10

---
+++ src/labs/a6/classes/index.js
@@ -0,0 +1,35 @@
+/*
+    Cesar Guerrero
+    10/16/22
+    CS5610 - Fall 2022
+
+    Assignment A6 - React
+*/
+
+import './index.css';
+
+function Classes() {
+    const color = 'blue';
+    const dangerous = false;
+    return (
+        <div>
+            <h2>Classes</h2>
+            <div className={`wd-bg-${color} wd-fg-black wd-padding-10px`}>
+                Dynamic Blue background
+            </div>
+            <div className={`${dangerous ? 'wd-bg-red' : 'wd-bg-green'} wd-fg-black wd-padding-10px`}>
+                Dangerous background
+            </div>
+            <div className="wd-bg-yellow wd-fg-black wd-padding-10px">
+                Yellow background
+            </div>
+            <div className="wd-bg-blue wd-fg-black wd-padding-10px">
+                Blue background
+            </div>
+            <div className="wd-bg-red wd-fg-black wd-padding-10px">
+                Red background
+            </div>
+        </div>
+    )
+};
+export default Classes;
